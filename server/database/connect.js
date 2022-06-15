82b22934ac9fdcc9fc8f61ff7635258a6a366da7
Nick Sooter <nick.sooter@gmail.com>
2022-06-15 09:50:44 -0600

    modified connection CLs, finishing back end

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+// require('dotenv').config()
 
 const connection = mongoose.connect(process.env.ATLAS_URI)
   .then(db => {
-    console.log('DB Connected. Mongo is happy!')
+    console.log('DB Connected Successfully...')
     return db;
   }).catch(err => {
-    console.log('Error Connecting. Mongo is sad.');
+    console.log('Error Connecting...');
   })
 
 module.exports = connection;
